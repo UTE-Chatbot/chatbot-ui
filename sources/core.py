e794a40daf171ea2633b6fda742fa604f0bd5d40
MuoiNgoc <vanthimuoingoc@gmail.com>
2025-03-02 19:43:59 +0700

    code này chạy được

--- sources/core.py
+++ sources/core.py
@@ -10,18 +10,17 @@ import os
 
 df = pd.read_csv("./sources/update_data.csv", header=None, names=["chunk"])
 
+
 EMBEDDING_MODEL = "BAAI/bge-m3"
 embedding_model = SentenceTransformer(EMBEDDING_MODEL)
-embeddings = np.array(embedding_model.encode(df["chunk"].tolist(), normalize_embeddings=True))
-
-
-
-INDEX_FILE = "faiss_index.bin"
+INDEX_FILE = "./sources/faiss_index_v1.bin"
 
 if os.path.exists(INDEX_FILE):
     index = faiss.read_index(INDEX_FILE)
     print("FAISS index loaded successfully.")
 else:
+    print("Embedding again")
+    embeddings = np.array(embedding_model.encode(df["chunk"].tolist(), normalize_embeddings=True))
     d, n = embeddings.shape
     index = faiss.IndexFlatIP(n)
     faiss.normalize_L2(embeddings)
@@ -29,22 +28,21 @@ else:
     faiss.write_index(index, INDEX_FILE)
     print("New FAISS index created and saved.")
 
-
 def retrieve_similar(query, top_k=3):
     query_embedding = embedding_model.encode([query], normalize_embeddings=True)
     _, indices = index.search(query_embedding, top_k)
-    return [(df.iloc[i]["title"], df.iloc[i]["content"]) for i in indices[0]]
+    return [df.iloc[i]["chunk"] for i in indices[0]]
 
 def generate_answer(query):
     retrieved_docs = retrieve_similar(query, 4)
 
-    context = "\n".join([f"{content}" for content in retrieved_docs])
+    context = "\n".join( retrieved_docs)
 
-    # print(context)
-    print('retrieved_context: \n: ', context)
-    print('\n\n\ --- \n\n')
+    print(context)
+    #print('retrieved_context: \n: ', context)
+    #print('\n\n\ --- \n\n')
     
-    prompt = f"\n{context}\n\Câu hỏi: {query}\nCâu trả lời:"
+    prompt = f"\Câu hỏi: {query}\nCâu trả lời:"
     response = ollama.chat(model='hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF', messages=[{"role": "user", "content": prompt}])
     return response["message"]["content"]
 
